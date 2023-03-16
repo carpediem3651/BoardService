5a016aa53a3e27a111619e99482bcb6ee24e6146
carpediem3651 <carpediem7490@gmail.com>
2023-03-16 16:30:45 +0900

    게시글 삭제 구현

--- src/main/java/com/fastcampus/ch4/controller/BoardController.java
+++ src/main/java/com/fastcampus/ch4/controller/BoardController.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.HashMap;
 import java.util.List;
@@ -21,6 +23,29 @@ import java.util.Map;
 public class BoardController {
     @Autowired
     BoardService boardService;
+
+    @PostMapping("/remove")
+    public String remove(Integer bno, Integer page, Integer pageSize, Model m, HttpSession session, RedirectAttributes rattr) {
+        String writer = (String) session.getAttribute("id");
+
+        try {
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+
+            int rowCnt = boardService.remove(bno, writer);
+
+            // 삭제 확인을 위한 코드
+            if(rowCnt!=1)
+                throw new Exception("board remove error");
+
+                rattr.addFlashAttribute("msg", "DEL_OK");
+            } catch (Exception e) {
+                e.printStackTrace();
+                rattr.addFlashAttribute("msg", "DEL_ERR");
+            }
+
+        return "redirect:/board/list";
+    }
 
     @GetMapping("/read")
     public String read(Integer bno, Integer page, Integer pageSize, Model m) {
